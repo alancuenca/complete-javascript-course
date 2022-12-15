65cb3215756610a7ab8750cda32a0b820d4fd6da
Alan Cuenca <alan.r.cuenca@gmail.com>
2022-12-15 13:33:13 -0800

    final

--- 18-forkify/final/src/js/config.js
+++ 18-forkify/final/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '1ea8da55-5497-438f-ac20-39c4f214b0a0';
 export const MODAL_CLOSE_SEC = 2.5;
